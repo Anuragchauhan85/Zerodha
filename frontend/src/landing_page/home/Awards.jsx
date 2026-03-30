e801da5d04a3d2d6edc7ee680502d37d663baae3
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-03-30 16:31:06 +0530

    code of home components

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -2,8 +2,56 @@ import React from 'react'
 
 const Awards = () => {
   return (
-    <div>Awards</div>
-  )
+    <div className="containern mt-5 mb-5 mx-5">
+      <div className="row ">
+        <div className="col-6 p-5">
+          <img src="media\images\largestBroker.svg" alt="Largest broker" />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>Largest Stock broker in India</h1>
+          <p className='mb-5'>
+            2+ million Zerodha clients contribute to ober 15% of all retail order
+            <br /> volumes in india daily by trading and investing in:
+          </p>
+          <div className="row">
+            <div className="col-6">
+              {" "}
+              <ul>
+                <li>
+                  <p>Futures and Options</p>
+                </li>
+                <li>
+                  <p>Commodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6">
+              {" "}
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Bonds and Govt. Securities</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img
+            src="media\images\pressLogos.png"
+            alt="Press logo"
+            style={{width:"90%"}}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards
--- frontend/src/landing_page/home/Stats.jsx
+++ frontend/src/landing_page/home/Stats.jsx
@@ -2,8 +2,52 @@ import React from 'react'
 
 const Stats = () => {
   return (
-    <div>Stats</div>
-  )
+    <div className="container p-5 ">
+      <div className="row p-5">
+        <div className="col-6 p-5">
+          <h1 className="fs-2 mb-5">Trust with confidence</h1>
+          <h2 className="fs-4">Customer-first always</h2>
+          <p className="text-muted">
+            That's why 1.3+ crore customers trust Zerodha with ₹3.5 <br />
+            lakh crores worth of equity investments.
+          </p>
+          <h2 className="fs-4">No spam or gimmicks</h2>
+          <p className="text-muted">
+            No gimmiks, spam, "gamification", or annoying push <br />{" "}
+            notifications. High quality apps that you use at your pace, <br />{" "}
+            the way you like.
+          </p>
+          <h2 className="fs-4">The Zerodha universe</h2>
+          <p className="text-muted">
+            Not just an app, but a whole ecosystem. out investment <br /> in 30+
+            fintech startups offer you tailored services specific <br /> to your
+            needs.
+          </p>
+          <h2 className="fs-4">Do better with money</h2>
+          <p className="text-muted">
+            With initiatives like Nudge and kill Switch, we don't just <br />{" "}
+            faciliate transactions, but actively help you do better with <br />{" "}
+            your money.
+          </p>
+        </div>
+        <div className="col-6 p-5">
+          <img
+            src="media\images\ecosystem.png"
+            alt="ecosystem"
+            style={{ width: "100%" }}
+          />
+          <div className="text-center mt-1">
+            <a href="" className="mx-5" style={{ textDecoration: "none" }}>
+              Explore our products <i class="fa-solid fa-arrow-right-long"></i>
+            </a>
+            <a href=" " style={{ textDecoration: "none" }}>
+              Try kite Demo <i class="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stats
