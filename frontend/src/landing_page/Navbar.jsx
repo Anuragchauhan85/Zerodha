304bd929476bac6309b688c81e87140225a175af
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-03-30 23:55:59 +0530

    all component home code complete

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav
+        className="navbar navbar-expand-lg border-bottom  "
+        style={{ backgrounColor: "#ffffff" }}
+      >
+        <div className="container p-2">
+          <a className="navbar-brand" href="#">
+            <img
+              src="media\images\logo.svg"
+              alt="logo"
+              style={{ width: "25%" }}
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse " 
+            id="navbarSupportedContent"
+          >
+            <form className="d-flex" role="search">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Signup
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Product
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Support
+                  </a>
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
