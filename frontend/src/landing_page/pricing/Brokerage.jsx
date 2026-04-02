b5019a2e1d3f9c673f77934bf65fd9681fae67bc
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-04-02 16:27:49 +0530

    pricing completed

--- frontend/src/landing_page/pricing/Brokerage.jsx
+++ frontend/src/landing_page/pricing/Brokerage.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 const Brokerage = () => {
   return (
-    <div>Brokerage</div>
-  )
+    <div className="container">
+      <div className="row p-5 mt-5 text-center border-top">
+        <div className="col-8 p-4 ">
+          <h3 className="fs-5" style={{ color: "#0080FF" }}>
+            Brokerage calculator
+          </h3>
+          <ul className="text-muted" style={{ textAlign:"left", lineHeight:"2.5",fontSize:"12px" }}>
+            <li>
+              Call & Trade and RMS auto-sqareoff: Additional charges of ₹50 +
+              GST per order.
+            </li>
+            <li>Digital contract notes will be sent via e-mail.</li>
+            <li>
+              Physical copies of contract notes, if required, shall be charged
+              ₹0 per contract note. Courier charges apply.
+            </li>
+            <li>
+              For NRI account (non-PIS), 0.5% or ₹100 per executed order for
+              equity (whichever is lower).
+            </li>
+            <li>
+              For NRI account (PIS), 0.5% or ₹200 per executed order for equity
+              (whichever is lower).
+            </li>
+            <li>
+              if the accoutn is in debt balance, any order placed will be
+              charged ₹40 per executed order instead of ₹20 per executed order.
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 p-4">
+          <h3 className="fs-5" style={{ color: "#0080FF" }}>
+            List of charges
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Brokerage
--- frontend/src/landing_page/pricing/PricingPage.jsx
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Brokerage from './Brokerage';
 import Hero from "./Hero";
+import OpenAccount from '../OpenAccount'
 
 const PricingPage = () => {
   return (
     <>
       <Hero />
+      <OpenAccount />
       <Brokerage />
     </>
   );
