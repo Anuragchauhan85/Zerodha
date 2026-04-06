d7a8d6b7f2962c7441981a4b00aa07a268f52b00
Anurag chauhan <Anuragchauhan8510@gmail.com>
2026-04-06 22:40:48 +0530

    using chart

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -8,10 +8,38 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { watchlist } from "../data/data";
-
-
+import { DoughnutChart } from "./DoughnutChart";
 
 const WatchList = () => {
+  const labels = watchlist.map((subArray) => subArray["name"]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Price",
+        data: watchlist.map((stock) => stock.price),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <div className="watchlist-container">
       <div className="search-container">
@@ -26,10 +54,11 @@ const WatchList = () => {
       </div>
 
       <ul className="list">
-        {watchlist.map((stock, index) => {return(
-          <WatchListItem stock={stock} key={index}/>)
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
         })}
       </ul>
+      <DoughnutChart data={data} />
     </div>
   );
 };
@@ -39,13 +68,13 @@ export default WatchList;
 const WatchListItem = ({ stock }) => {
   const [showWatchlistActions, SetShowWatchlistActions] = useState(false);
 
-  const handleMouseEnter = (e) => {
+  const handleMouseEnter = () => {
     SetShowWatchlistActions(true);
-  }
+  };
 
-  const handleMouseExit = (e) => {
+  const handleMouseExit = () => {
     SetShowWatchlistActions(false);
-  }
+  };
 
   return (
     <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
@@ -61,14 +90,12 @@ const WatchListItem = ({ stock }) => {
           <span className="price">{stock.price}</span>
         </div>
       </div>
-      {showWatchlistActions && <WatchListActions uid={stock.name}/>}
+      {showWatchlistActions && <WatchListActions uid={stock.name} />}
     </li>
   );
 };
 
-
 const WatchListActions = ({ uid }) => {
-
   const generalContext = useContext(GeneralContext);
 
   const handleBuyClick = () => {
@@ -113,4 +140,4 @@ const WatchListActions = ({ uid }) => {
       </span>
     </span>
   );
-}
+};
